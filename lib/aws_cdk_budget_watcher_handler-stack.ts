78e97a6cd2cb4a6d9c3f70c6ad0d46b97d479185
andrew lau <alau9998@hotmail.com>
2023-04-12 17:12:02 -0400

    project init

--- lib/aws_cdk_budget_watcher_handler-stack.ts
+++ lib/aws_cdk_budget_watcher_handler-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions'
+import * as budgets from 'aws-cdk-lib/aws-budgets';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as nodejs  from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as stepfunctionstasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+
 
 export class AwsCdkBudgetWatcherHandlerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    cdk.Tags.of(this).add('AwsCdkBudgetWatcherHandlerStack', '')
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkBudgetWatcherHandlerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new sns.Topic(this, 'BudgetWatcher', {
+      displayName: 'Budget Wacther Alert',
+    });
+
   }
 }
