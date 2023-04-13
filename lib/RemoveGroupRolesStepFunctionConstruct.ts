f41ef7b4763aef60f861685480863f31ce877fcd
andrew lau <alau9998@hotmail.com>
2023-04-13 16:45:40 -0400

    april 13 2023

--- lib/RemoveGroupRolesStepFunctionConstruct.ts
+++ lib/RemoveGroupRolesStepFunctionConstruct.ts
@@ -1,21 +1,55 @@
 import { Construct } from 'constructs';
 
 import * as nodejs  from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as iam from 'aws-cdk-lib/aws-iam'
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
 import * as stepfunctionstasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
-export interface RemoveGroupRolesStepFunctionProp {
+export interface DowngradeGroupRolesStepFunctionProp {
     nameOfCognitoGroupToDowngrade: string;
-    identityPoolId: string;
+    cognitoPoolId: string;
     roleToDowngradeTo: iam.IRole;
 };
 
 export class RemoveGroupRolesStepFunction extends Construct {
 
-    constructor(scope: Construct, id: string, props: RemoveGroupRolesStepFunctionProp) {
+    constructor(scope: Construct, id: string, props: DowngradeGroupRolesStepFunctionProp) {
         super(scope, id);
 
+
+        const step1Function = new nodejs.NodejsFunction(this, 'step1_remove_grp_role_lambda', {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            environment: {
+                nameOfCognitoGroupToDowngrade: props.nameOfCognitoGroupToDowngrade,
+                cognitoPoolId: props.cognitoPoolId,
+                roleToDowngradeToArn: props.roleToDowngradeTo.roleArn
+            }
+        });
+
+        step1Function.role?.attachInlinePolicy(
+            new iam.Policy(this, 'removegrp-userpool-iam-policy', {
+              statements: [new iam.PolicyStatement({
+                actions: [
+                  "iam:GetRole",
+                  "iam:PassRole"  
+                ],
+                resources: [
+                  props.roleToDowngradeTo.roleArn
+                ],
+              })],
+            })
+          );
+
+        const cognitoPool = cognito.UserPool.fromUserPoolId(this, "myCognitoUserPool", 
+            props.cognitoPoolId);
+
+        cognitoPool.grant(step1Function,
+            "cognito-idp:ListGroups",
+            "cognito-idp:GetGroup",
+            "cognito-idp:ListUserPools"
+            );
 
     }
 
--- lib/aws_cdk_budget_watcher_handler-stack.step1_remove_grp_role_lambda.ts
+++ lib/aws_cdk_budget_watcher_handler-stack.step1_remove_grp_role_lambda.ts
@@ -2,21 +2,26 @@ import { Context } from 'aws-lambda';
 
 import { CognitoIdentityClient, CreateIdentityPoolCommand, ListIdentitiesCommand, 
     ListIdentitiesCommandOutput,
-    //https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-cognito-identity/classes/getidentitypoolrolescommand.html
+
     GetIdentityPoolRolesCommand,
-    //https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-cognito-identity/interfaces/getidentitypoolrolescommandoutput.html
+
     GetIdentityPoolRolesCommandInput,
     GetIdentityPoolRolesCommandOutput,
 
-    //https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-cognito-identity/classes/setidentitypoolrolescommand.html
     SetIdentityPoolRolesCommand,
     SetIdentityPoolRolesCommandInput,
     SetIdentityPoolRolesCommandOutput
  } from "@aws-sdk/client-cognito-identity";
 
-//https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-cognito-identity-provider/
 import { CognitoIdentityProviderClient, AddCustomAttributesCommand, AdminAddUserToGroupCommand,
      ListGroupsCommand, ListIdentityProvidersCommand, GroupType,
         ProviderDescription, UpdateGroupCommand, UpdateGroupCommandOutput 
     } from "@aws-sdk/client-cognito-identity-provider";
 
+export const handler = async (event: any, context: Context): Promise<any> => {
+
+    const identityProviderclient = new CognitoIdentityProviderClient({ region: process.env.AWS_REGION });
+
+    
+
+}
